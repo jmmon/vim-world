db867957e31b6054856d934caf8d73dee450e296
jmmon <jmmon93@gmail.com>
2026-01-31 08:26:40 -0500

    small style fixes easier to navigate

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,26 +3,26 @@ import { Link, type DocumentHead } from "@builder.io/qwik-city";
 import Inventory from "~/components/inventory/inventory";
 
 export default component$(() => {
-  return (
-    <>
-      <Link href="/canvas1">Canvas 1</Link>
-      <h1>Hi 👋</h1>
-      <div>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </div>
-      <Inventory />
-    </>
-  );
+    return (
+        <div class="p-16">
+            <Link class="text-3xl" href="/canvas1">Canvas 1</Link>
+            <h1>Hi 👋</h1>
+            <div>
+                Can't wait to see what you build with qwik!
+                <br />
+                Happy coding.
+            </div>
+            <Inventory />
+        </div>
+    );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
-  meta: [
-    {
-      name: "description",
-      content: "Qwik site description",
-    },
-  ],
+    title: "Welcome to Qwik",
+    meta: [
+        {
+            name: "description",
+            content: "Qwik site description",
+        },
+    ],
 };
