b70df7ce3292bfcb91acc700a6d86ecef396a326
jmmon <jmmon93@gmail.com>
2026-01-31 14:12:11 -0500

    roundToDecimals util

---
+++ src/utils/utils.ts
@@ -0,0 +1,8 @@
+const ROUND_TO_DECIMALS_MIN = 0;
+const ROUND_TO_DECIMALS_MAX = 4;
+export function roundToDecimals(value: number, decimals: number = 2) {
+    if (decimals < ROUND_TO_DECIMALS_MIN) decimals = ROUND_TO_DECIMALS_MIN;
+    if (decimals > ROUND_TO_DECIMALS_MAX) decimals = ROUND_TO_DECIMALS_MAX;
+    const factor = 10 ** decimals;
+    return Math.round(value * factor) / factor;
+}
