013d945f724136db0328d4eb1a69fef7768ce68c
jmmon <jmmon93@gmail.com>
2026-02-03 06:10:39 -0500

    list out fsm yank paste keys

--- src/fsm/types.ts
+++ src/fsm/types.ts
@@ -1,15 +1,31 @@
 export type VimMode = "normal" | "operator" | "awaitingChar" | "command";
+export type VimActionType = "MOVE" | "INTERACT" | "TARGET" | "COMMAND";
+export type OperatorKey = "y" | "d" | "c" | "p";
+export type AwaitingCharKey = "f" | "F" | "t" | "T" | "g";
+export type MovementKey = "h" | "j" | "k" | "l" | "w" | "b";
+export type ModifierKey = "a" | "i";
+export type TargetKey =
+    | "["
+    | "]"
+    | "{"
+    | "}"
+    | "("
+    | ")"
+    | "<"
+    | ">"
+    | '"'
+    | "'"
+    | "`";
 
 export interface VimFSMState {
     mode: VimMode;
     count: number | null;
-    operator?: "y" | "d" | "c"; // more?
-    motion?: string; // "f" | "F" // more?
     buffer: string[];
+    operator?: OperatorKey; // more?
+    motion?: string; // "f" | "F" // more?
     lastAction?: VimAction;
 }
 
-export type VimActionType = "MOVE" | "INTERACT" | "TARGET" | "COMMAND";
 export interface VimAction {
     type: VimActionType;
     key?: string;
@@ -27,5 +43,3 @@ export type TransitionFn = (
     event: KeyboardEvent,
     lastAction: null | VimAction,
 ) => TransitionResult;
-
-
